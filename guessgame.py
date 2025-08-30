43fb6e9591048efb839d98eed6b0e1f3a583653f
2001-Rahul <rahulbnair.bsccs2020@gmail.com>
2025-08-30 12:39:45 +0530

    guessgame code changes

--- guessgame.py
+++ guessgame.py
@@ -3,19 +3,23 @@ choosen=random.randint(1,100)
 nguess=0
 userguess=None
 while userguess!=choosen:
-    userguess=int(input('enter your guess '))
-    nguess+=1
-    
-    if userguess==choosen:
-        print(f"you won correct guess...number of guess={nguess}")
-    else:
-        if userguess<choosen:
-            print ("enter a larger number")
-        else:
-            print("enter a smaller number")        
+    try:
 
-with open("highscore.txt") as f:
-    guessfile=int(f.read())
-    if guessfile>nguess:
-        with open("highscore.txt","w")as f:
-            f.write(str(nguess))
+        userguess=input('enter your guess ')
+        nguess+=1
+        userguess=int(userguess)
+        if userguess==choosen:
+             print(f"you won correct guess...number of guess={nguess}")
+        else:
+             if userguess<choosen:
+                  print ("enter a larger number")
+             else:
+                 print("enter a smaller number")        
+
+        with open("highscore.txt") as f:
+         guessfile=int(f.read())
+         if guessfile>nguess:
+              with open("highscore.txt","w")as f:
+                   f.write(str(nguess))
+    except Exception as e:
+        print(f"you have done an error: {e}")               
